b4d35812c3c676f1f60bd761b3bc24707e0b8abf
EnzoAguero <agueroenzo10@gmail.com>
2021-05-12 14:26:30 -0300

    mi primer commit

---
+++ git.js
@@ -0,0 +1,3 @@
+//Comandos importantes:
+//con git init nos hace un repositorio local donde podemos trabajar tranquilamente 
+//la u significa es que esta sin seguimiento, por eso hay que guardarlo con el git add . ; lo que se hace con esto es ponerlo en una "caja" todavia no esta en el repositorio
